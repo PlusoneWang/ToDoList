ecad15115419ebe40fe3a9e1a9a83865a5c6fcd7
Plusone <plusonewang82@gmail.com>
2018-06-05 17:29:01 +0800

    Add: 功能_新增清單_未完

---
+++ ToDoList_MVC5_Vue.Service/ToDoListService.cs
@@ -0,0 +1,45 @@
+﻿namespace ToDoList_MVC5_Vue.Service
+{
+    using System;
+    using System.Linq;
+
+    using Po.Result;
+
+    using ToDoList_MVC5_Vue.Library.Models;
+
+    using Guid = Ci.Sequential.Guid;
+
+    public class ToDoListService : BaseService
+    {
+        /// <summary>
+        /// 使用清單名稱、帳號，新增清單
+        /// </summary>
+        /// <param name="listName">清單名稱</param>
+        /// <param name="account">帳號</param>
+        /// <returns>新增結果</returns>
+        public PoResult CreateList(string listName, string account)
+        {
+            try
+            {
+                var user = this.Database.Users.FirstOrDefault(o => o.Account == account);
+                if (user == null)
+                    return PoResult.DbNotFound();
+
+                this.Database.ToDoLists.Add(new ToDoList
+                {
+                    Id = Guid.Create(),
+                    Name = listName,
+                    UserId = user.Id,
+                    Sort = user.ToDoLists.Count
+                });
+
+                this.Database.SaveChanges();
+                return PoResult.PoSuccess();
+            }
+            catch (Exception e)
+            {
+                return PoResult.Exception(e);
+            }
+        }
+    }
+}
--- ToDoList_MVC5_Vue.Web/Controllers/ToDoListController.cs
+++ ToDoList_MVC5_Vue.Web/Controllers/ToDoListController.cs
@@ -2,11 +2,36 @@
 {
     using System.Web.Mvc;
 
+    using ToDoList_MVC5_Vue.Service;
+
     public class ToDoListController : BaseController
     {
+        private readonly ToDoListService toDoListService;
+
+        public ToDoListController()
+        {
+            this.toDoListService = new ToDoListService();
+        }
+
+        /// <summary>
+        /// ToDoList主頁面
+        /// </summary>
+        /// <returns>主頁面</returns>
         public ActionResult Index()
         {
+            // TODO model
             return this.View();
+        }
+
+        /// <summary>
+        /// 建立清單
+        /// </summary>
+        /// <param name="listName">清單名稱</param>
+        /// <returns>建立結果</returns>
+        public JsonResult CreateList(string listName)
+        {
+            var createResult = this.toDoListService.CreateList(listName, this.CurrentUser.Id);
+            return this.Json(createResult);
         }
     }
 }
